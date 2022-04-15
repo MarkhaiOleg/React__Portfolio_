aeb8da0b0e70bf7d562cb213779f39020f0562b6
Oleg Markhai <oleg987663@gmail.com>
2022-04-15 23:15:41 +0300

    made a color switch on the site

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Toggle from '../Toggle/Toggle'
 import './Navbar.css'
 
 const Navbar = () => {
@@ -6,7 +7,7 @@ const Navbar = () => {
         <nav className="n-wrapper">
             <div className="n-left">
                 <div className="n-name">Oleg</div>
-                <span>toggle</span>
+                <Toggle />
             </div>
             <div className="n-right">
                 <div className="n-list">
